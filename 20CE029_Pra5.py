e4af4bf5bd00a2446ecd06518412655d13fe0f84
MisariGami <92617613+MisariGami@users.noreply.github.com>
2022-02-04 17:32:19 +0530

    Add files via upload

---
+++ 20CE029_Pra5.py
@@ -0,0 +1,23 @@
+# 20CE029 Misari Gami
+# GitHub repository link : 
+
+# You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa. 
+
+# Sample Input: HackerRank.com presents "Pythonist 2".
+
+# Sample Output: hACKERrANK.COM PRESENTS "pYTHONIST 2".
+
+
+def swap_case(str):
+    ans = ''
+    for i in str:
+        if i.isupper():
+            ans += i.lower()
+        elif i.islower():
+            ans += i.upper()
+        else:
+            ans += i
+    return ans
+
+str = input()
+print(swap_case(str))
